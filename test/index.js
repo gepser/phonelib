8743fd142ac5c12934d0eb86ac7860d5ae2791bf
gepser <gepser.hoil@xoom.com>
2015-08-19 09:36:21 -0600

    Added standard javascript library and parsed all the project

--- test/index.js
+++ test/index.js
@@ -1,46 +1,47 @@
-var Code = require('code');
-var Lab = require('lab');
-var expect = Code.expect;
-var lab = exports.lab = Lab.script();
+var Code = require('code')
+var Lab = require('lab')
+var expect = Code.expect
+var lab = exports.lab = Lab.script()
 
-var phonelib = require('../');
+var phonelib = require('../')
 
 lab.experiment('Check numbers', function () {
-
   lab.test('Check a valid phone number using phonelib', function (done) {
+    var phone = '2024561414'
+    var country = 'US'
 
-      var phone = '2024561414';
-      var country = 'US';
+    var expected = {
+      international: '+1 202-456-1414',
+      isValid: true
+    }
 
-      var expected = {
-          international: '+1 202-456-1414',
-          isValid: true
-      };
+    phonelib.isValid({phone: phone, country: country}, function (err, phoneNumber) {
+      if (err) {
+        done(err)
+      }
+      expect(phoneNumber.international).to.be.equal(expected.international)
+      expect(phoneNumber.isValid).to.be.equal(expected.isValid)
+      done()
+    })
 
-      phonelib.isValid({phone:phone, country:country}, function(err, phoneNumber){
-
-       expect(phoneNumber.international).to.be.equal(expected.international);
-       expect(phoneNumber.isValid).to.be.equal(expected.isValid);
-       done();
-      });
-
-  });
+  })
 
   lab.test('Check an invalid phone number using phonelib', function (done) {
+    var phone = '88888888'
+    var country = 'GT'
 
-      var phone = '88888888';
-      var country = 'GT';
+    var expected = {
+      isValid: false
+    }
 
-      var expected = {
-          isValid: false
-      };
+    phonelib.isValid({phone: phone, country: country}, function (err, phoneNumber) {
+      if (err) {
+        done(err)
+      }
+      expect(phoneNumber.isValid).to.be.equal(expected.isValid)
+      done()
+    })
 
-      phonelib.isValid({phone:phone, country:country}, function(err, phoneNumber){
+  })
 
-       expect(phoneNumber.isValid).to.be.equal(expected.isValid);
-       done();
-      });
-
-  });
-
-});
+})
